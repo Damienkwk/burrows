02b7ac62f77b739fdfa9f6f1304b1cb50882b262
Eewei Seah <seaheewei@gmail.com>
2023-02-27 20:58:10 +0800

    bookmark feature can theoretically toggle on the index page now

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,7 +5,17 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    raise
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.user = current_user
+    unless @bookmark.save
+      Bookmark.find_by(user: current_user, flat: @bookmark.flat).destroy
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:flat_id)
   end
 
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :flat
+
+  validates :user, uniqueness: { scope: :flat }
 end
