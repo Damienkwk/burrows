d3619a25319609275b2ca687240d4d7b12dd294c
Eewei Seah <seaheewei@gmail.com>
2023-03-01 20:20:06 +0800

    added create function to room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,27 @@
 class RoomsController < ApplicationController
+  before_action :set_flat, only: [:new, :create]
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    @room.flat = @flat
+    if @room.save
+      redirect_to flat_room_path(@flat, @room)
+    else
+      render new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:flat_id])
+  end
 end
