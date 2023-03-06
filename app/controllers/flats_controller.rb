ab39bd0c0aa6cc94ff46fb6480b4a851172d5fb3
Lester Ho <lesterhjy@gmail.com>
2023-03-06 21:24:12 +0800

    Adjusting authorisation and authentication for flats index and show pages

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,6 @@
 class FlatsController < ApplicationController
   before_action :set_flat, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @flats = policy_scope(Flat)
