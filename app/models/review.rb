eb25b0d02b9efc6dba7df55705d17d56aa850daa
Damien Koh <damienkwk@hotmail.sg>
2023-03-11 12:14:18 +0800

    add-review

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ApplicationRecord
   belongs_to :user
   validates :title, :content, :rating, presence: true
   validates :rating, inclusion: { in: [1, 2, 3, 4, 5], allow_nil: false }
+  validates :content, length: { minimum: 20 }
 end
